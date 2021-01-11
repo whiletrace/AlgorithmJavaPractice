1b4d746ef430f19ee5af4196afdad6ed923b6fd5
Trace Harris <traceh@whiletrace.com>
2021-01-11 01:40:31 -0800

    my first java program and my first study of a algorithm in java

---
+++ QuickFindUF.java
@@ -0,0 +1,42 @@
+// *****************************************************************************
+// *  Name:              Ada Lovelace
+// *  Coursera User ID:  123456
+// *  Last modified:     October 16, 1842
+// ****************************************************************************
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class QuickFindUF {
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pid) id[i] = qid;
+    }
+
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
+    }
+}
